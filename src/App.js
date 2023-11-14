5a2aaaab7265e29084d23761b4883bb63b0ec5ef
Lucas Domingues <lucasdomingues.iftm@gmail.com>
2023-11-14 05:06:37 -0300

    alteracoes tela de inicio e configuracao de layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import VideoSwitcher from './components/VideoSwitcher/VideoSwitcher';
+import bio from './assets/images/bio.jpeg'; // Importe a imagem
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Retículo Endoplasmático rugoso </h1>
-      </header>
-      <main>
-        <VideoSwitcher />
-      </main>
-    </div>
-  );
+  const [isVideoStarted, setIsVideoStarted] = useState(false);
+
+    const handleStart = () => {
+      setIsVideoStarted(true); // Torna o vídeo visível
+        // Outras ações de início, se necessário
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Retículo Endoplasmático rugoso</h1>
+            </header>
+            <main>
+                {!isVideoStarted &&  (
+                    <>
+                        <img src={bio} alt="Descrição da imagem" className="bio-image" />
+                        <button onClick={handleStart} className="start-button">Iniciar</button>
+                    </>
+                )}
+                {isVideoStarted && <VideoSwitcher isStarted={isVideoStarted}/>}
+            </main>
+        </div>
+    );
 }
 
 export default App;
--- src/components/VideoSwitcher/VideoSwitcher.jsx
+++ src/components/VideoSwitcher/VideoSwitcher.jsx
@@ -1,140 +1,103 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useMemo } from 'react';
 import './VideoSwitcher.css';
-// Importe todos os seus vídeos
 import video1 from '../../assets/videos/video1(2).mp4';
 import video2 from '../../assets/videos/video2(2).mp4';
-//import video3 from '../../assets/videos/video3(2).mp4';
 import video4 from '../../assets/videos/video4(2).mp4';
 import video5 from '../../assets/videos/video5(2).mp4';
 import video6 from '../../assets/videos/video6(2).mp4';
 import scissorsImage from '../../assets/images/scissors.png';
 
-const VideoSwitcher = () => {
-  // Lista de vídeos a serem reproduzidos
-  const videoList = [video1, video2, video4, video5, video6];
-  const [isAwaitingTouch, setIsAwaitingTouch] = useState(false); // Novo estado para controlar o toque após vídeo 2
-
-  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
-  const [showElements, setShowElements] = useState(false);
-  const [animationStarted, setAnimationStarted] = useState(false);
-  const videoRef = useRef(null);
-
-  useEffect(() => {
-    console.log('useEffect inicial: Configurando para mostrar elementos após 3 segundos');
-    const timer = setTimeout(() => {
-      console.log('Mostrando elementos (tesoura e linhas)');
-      setShowElements(true);
-    }, 3000);
-
-    return () => {
-      console.log('Limpando timer');
-      clearTimeout(timer);
-    };
-  }, []);
-
-  const handleScissorsClick = () => {
-    console.log('Clicou na tesoura');
-    if (showElements && !animationStarted) {
-      console.log('Iniciando animação da tesoura');
-      setAnimationStarted(true);
-      videoRef.current.play();
-    } else {
-      console.log('Animação já iniciada ou elementos ainda não estão prontos para serem mostrados');
-    }
-  };
-
-  const handleAnimationEnd = () => {
-    console.log('Animação da tesoura terminou.');
-    if (animationStarted) {
-      console.log('Verificando se é hora de tocar o vídeo');
-      if (currentVideoIndex === 0) {
-        console.log('Tentando iniciar a reprodução do vídeo 1');
-        videoRef.current.play(); // Aqui deve iniciar a reprodução do vídeo
+const VideoSwitcher = ({ isStarted }) => {
+  const videoList = useMemo(() => [video1, video2, video4, video5, video6], []);
+    const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+    const [showElements, setShowElements] = useState(false);
+    const [animationStarted, setAnimationStarted] = useState(false);
+    const [isAwaitingTouch, setIsAwaitingTouch] = useState(false);
+    const videoRef = useRef(null);
+    useEffect(() => {
+      if (isStarted) {
+          videoRef.current.load();
+          setShowElements(true); // Por exemplo
       }
-      setAnimationStarted(false);
-      setShowElements(false);
-    } else {
-      console.log('handleAnimationEnd chamado, mas a animação não foi iniciada.');
-    }
-  };
+  }, [isStarted]);
+    const handleScissorsClick = () => {
+        if (showElements && !animationStarted) {
+            setAnimationStarted(true);
+            videoRef.current.play(); // Inicia a reprodução do vídeo após o clique na tesoura
+        }
+    };
 
-  const handleVideoEnd = () => {
-    console.log(`Vídeo ${currentVideoIndex} terminou.`);
-    if (currentVideoIndex === 0) {
-      // Se for o primeiro vídeo, simplesmente avance para o segundo.
-      console.log('Avançando automaticamente para o vídeo 1.');
-      setCurrentVideoIndex(1);
-    } else if (currentVideoIndex < videoList.length - 1) {
-      // Se não for o último vídeo, aguarde a interação do usuário para continuar.
-      console.log(`Esperando pelo toque na tela após o vídeo ${currentVideoIndex}.`);
-      setIsAwaitingTouch(true);
-    } else {
-      // Último vídeo terminou, trate conforme necessário.
-      console.log('Todos os vídeos foram reproduzidos.');
-    }
-  };
+    const handleAnimationEnd = () => {
+        setAnimationStarted(false);
+        setShowElements(false);
+        if (currentVideoIndex === 0) {
+            setCurrentVideoIndex(1);
+        }
+    };
 
-  
+    const handleVideoEnd = () => {
+      console.log(`Vídeo ${currentVideoIndex} terminou.`);
+      if (currentVideoIndex < videoList.length - 1) {
+          // Se não for o último vídeo, defina isAwaitingTouch para true
+          console.log(`Esperando pelo toque na tela após o vídeo ${currentVideoIndex}.`);
+          setIsAwaitingTouch(true);
+      } else {
+          // Último vídeo terminou, trate conforme necessário.
+          console.log('Todos os vídeos foram reproduzidos.');
+          // Aqui, você pode definir o comportamento para quando todos os vídeos tiverem sido reproduzidos
+      }
+  };
 
   const handleTouch = () => {
     console.log('Tela tocada.');
-    if (isAwaitingTouch && currentVideoIndex > 0) {
-      advanceToNextVideo();
+    if (isAwaitingTouch) {
+        const nextVideoIndex = currentVideoIndex + 1;
+        if (nextVideoIndex < videoList.length) {
+            console.log(`Avançando para o vídeo ${nextVideoIndex}.`);
+            setCurrentVideoIndex(nextVideoIndex);
+            setIsAwaitingTouch(false);
+        }
     }
-  };
+};
 
-  const advanceToNextVideo = () => {
-    const nextVideoIndex = currentVideoIndex + 1;
-    if (nextVideoIndex < videoList.length) {
-      console.log(`Avançando para o vídeo ${nextVideoIndex}.`);
-      setCurrentVideoIndex(nextVideoIndex);
-      setIsAwaitingTouch(false);
-    }
-  };
-  
-  useEffect(() => {
-    console.log(`useEffect para troca de vídeo: índice atual do vídeo: ${currentVideoIndex}`);
-    if (currentVideoIndex > 0) {
-      console.log(`Carregando vídeo ${currentVideoIndex}`);
-      videoRef.current.src = videoList[currentVideoIndex];
-      videoRef.current.load();
-      videoRef.current.play();
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentVideoIndex]);
+    useEffect(() => {
+        if (currentVideoIndex > 0) {
+            videoRef.current.src = videoList[currentVideoIndex];
+            videoRef.current.load();
+            videoRef.current.play();
+        }
+    }, [currentVideoIndex, videoList]);
 
-  return (
-    <div className="video-container" onClick={isAwaitingTouch ? handleTouch : undefined}>
-      <video
-        ref={videoRef}
-        src={videoList[currentVideoIndex]}
-        loop={false}
-        muted
-        playsInline
-        className="video-player"
-        onEnded={handleVideoEnd}
-      />
-      {showElements && currentVideoIndex === 0 && (
-        <>
-          <div className="line vertical-line"></div>
-          <div className="line horizontal-line"></div>
-          <img
-            src={scissorsImage}
-            alt="Scissors"
-            className={`scissors ${animationStarted ? 'cutting' : ''}`}
-            onClick={handleScissorsClick}
-            onAnimationEnd={handleAnimationEnd}
-          />
-          {!animationStarted && <p className="message">Toque no objeto para iniciar</p>}
-        </>
-      )}
-      {isAwaitingTouch && currentVideoIndex > 0 && (
-        <div className="touch-prompt">
-          Toque aqui!
+    return (
+        <div className="video-container" onClick={isAwaitingTouch ? handleTouch : undefined}>
+            <video
+                ref={videoRef}
+                src={videoList[currentVideoIndex]}
+                loop={false}
+                muted
+                playsInline
+                className="video-player"
+                onEnded={handleVideoEnd}
+            />
+            {showElements && currentVideoIndex === 0 && (
+                <>
+                    <div className="line vertical-line"></div>
+                    <div className="line horizontal-line"></div>
+                    <img
+                        src={scissorsImage}
+                        alt="Scissors"
+                        className={`scissors ${animationStarted ? 'cutting' : ''}`}
+                        onClick={handleScissorsClick}
+                        onAnimationEnd={handleAnimationEnd}
+                    />
+                    {!animationStarted && <p className="message">Toque no objeto para iniciar</p>}
+                </>
+            )}
+            {isAwaitingTouch && currentVideoIndex > 0 && (
+                <div className="touch-prompt">Toque aqui!</div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 };
 
 export default VideoSwitcher;
